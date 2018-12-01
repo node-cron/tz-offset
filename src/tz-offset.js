ecd6a8cd0aa2a1dab76592f426317108b27b73e2
danielrussellla <danielrussell16@gmail.com>
2018-12-01 15:24:17 -0800

    update to arrow functions

--- src/tz-offset.js
+++ src/tz-offset.js
@@ -1,9 +1,9 @@
 'use strict'
 const offsets = require("../generated/offsets");
 
-module.exports = (function () {
+module.exports = (() => {
 
-    function offsetOf(timezone) {
+    const offsetOf = (timezone) => {
         const offset = offsets[timezone];
         if (offset != undefined && offset != null) {
             return offset;
@@ -12,20 +12,20 @@ module.exports = (function () {
         }
     }
 
-    function removeOffset(date) {
+    const removeOffset = (date) => {
         const currentOffset = date.getTimezoneOffset() * -60000;
         return date.getTime() - currentOffset;
     }
 
-    function timeAt(date, timezone) {
+    const timeAt = (date, timezone) => {
         const timeUtc = removeOffset(date);
         const offset = offsetOf(timezone) * -60000;
         return new Date(timeUtc + offset);
     }
 
     return {
-        offsetOf: offsetOf,
-        removeOffset: removeOffset,
-        timeAt: timeAt
+        offsetOf,
+        removeOffset,
+        timeAt
     };
 })();
