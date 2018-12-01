4086ad41ca6dece2b74b6728a73fdc355287f896
danielrussellla <danielrussell16@gmail.com>
2018-12-01 15:08:02 -0800

    -mfix strict mode errors with let/const in src/tz-offset.js

--- src/tz-offset.js
+++ src/tz-offset.js
@@ -1,24 +1,24 @@
-var offsets = require("../generated/offsets");
+const offsets = require("../generated/offsets");
 
-module.exports = (function() {
-    
-    function offsetOf(timezone){
-        var offset = offsets[timezone];
-        if(offset != undefined && offset != null){
+module.exports = (function () {
+
+    function offsetOf(timezone) {
+        let offset = offsets[timezone];
+        if (offset != undefined && offset != null) {
             return offset;
         } else {
-            throw Error("Invalid timezone "+ timezone);
+            throw Error("Invalid timezone " + timezone);
         }
     }
 
-    function removeOffset(date){
-        var currentOffset = date.getTimezoneOffset() * -60000;
+    function removeOffset(date) {
+        let currentOffset = date.getTimezoneOffset() * -60000;
         return date.getTime() - currentOffset;
     }
 
-    function timeAt(date, timezone){
+    function timeAt(date, timezone) {
         let timeUtc = removeOffset(date);
-        var offset = offsetOf(timezone) * -60000;
+        let offset = offsetOf(timezone) * -60000;
         return new Date(timeUtc + offset);
     }
 
