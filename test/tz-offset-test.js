06e61898b7c8fad399822ffdac6c19418b0d328e
danielrussellla <danielrussell16@gmail.com>
2018-12-01 15:36:58 -0800

    fix tests

--- test/tz-offset-test.js
+++ test/tz-offset-test.js
@@ -1,7 +1,8 @@
+'use strict'
 let tzOffset = require("../src/tz-offset");
-let {assert} = require("chai");
+let { assert } = require("chai");
 
-process.env.TZ= "America/Sao_Paulo";
+process.env.TZ = "America/Sao_Paulo";
 
 describe("tz-offset", () => {
     it("should return 180 to America/Sao_Paulo", () => {
@@ -24,15 +25,14 @@ describe("tz-offset", () => {
 
     it("should remove current offset", () => {
         let date = new Date();
-        utcTime = tzOffset.removeOffset(date);
+        let utcTime = tzOffset.removeOffset(date);
         assert.equal(utcTime, date.getTime() + date.getTimezoneOffset() * 60000);
     });
 
     it("should return the time at zone", () => {
         let date = new Date(2018, 8, 20, 0, 0, 0, 0);
         let expectedDate = new Date(2018, 8, 20, 3, 0, 0, 0);
-        result = tzOffset.timeAt(date, "Etc/UTC");
-        console.log(result);
+        let result = tzOffset.timeAt(date, "Etc/UTC");
         assert.equal(result.toString(), expectedDate.toString());
     });
 });
